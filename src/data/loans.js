8da9f6a0a224c6236a24308482ea960638148c18
N45h0 <70018907+N45h0@users.noreply.github.com>
2024-12-04 10:29:16 -0300

    Create loans.js

---
+++ src/data/loans.js
@@ -0,0 +1,65 @@
+// borra esta linea al pegar
+const loans = [
+  {
+    id: 1,
+    name: "Refinanciamiento Antel",
+    owner: "LAFIO",
+    capital: 4941.19,
+    installments: 9,
+    amount: 549.02,
+    paidInstallments: 1,
+    interestRate: 0,
+  },
+  {
+    id: 2,
+    name: "BROU Viaje Argentina",
+    owner: "LAFIO",
+    capital: 12000.00,
+    installments: 10,
+    amount: 1411.58,
+    paidInstallments: 0,
+    interestRate: 29.00,
+    moratory: 43.74,
+    cancellationFee: 732.00,
+    account: "2477"
+  },
+  {
+    id: 3,
+    name: "BROU Dentista",
+    owner: "Lovia",
+    capital: 20000.00,
+    installments: 12,
+    amount: 1916.39,
+    paidInstallments: 2,
+    interestRate: 23.00,
+    moratory: 51.35,
+    ceipRetention: true
+  },
+  {
+    id: 4,
+    name: "BROU Buenos Aires",
+    owner: "Lovia",
+    capital: 10000.00,
+    installments: 6,
+    amount: 1801.64,
+    paidInstallments: 0,
+    interestRate: 19.00,
+    moratory: 43.74,
+    ceipRetention: true
+  },
+  {
+    id: 5,
+    name: "Adelanto Sueldo BROU",
+    owner: "Lovia",
+    capital: 4800.00,
+    installments: 1,
+    amount: 4831.57,
+    paidInstallments: 0,
+    interestRate: 0,
+    moratory: 43.74,
+    isOverdue: true
+  }
+];
+
+export default loans;
+// pega aquí
