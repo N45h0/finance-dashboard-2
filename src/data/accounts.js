e5f763baae90396229e36842e57f579559c4f04e
N45h0 <70018907+N45h0@users.noreply.github.com>
2024-12-07 12:32:53 -0300

    Update accounts.js correción nombres y servicios de tarjetas

--- src/data/accounts.js
+++ src/data/accounts.js
@@ -4,22 +4,22 @@ const accounts = [
    name: "Brou Débito 6039",
    type: "Visa débito",
    expiry: "03/2029",
-   income: ["Pagos Expande Digital", "Ex sueldo Elared"],
+   income: ["Pagos Expande Digital"],
    services: [
      "Spotify Premium Familiar",
      "Claude Pro", 
      "Google One",
      "Plan Antel",
      "Refinanciamiento Antel"
-   ]
+   ],
+     linkedLoans: ["BROU Viaje Argentina", "BROU Dentista", "BROU Buenos Aires"]
  },
  {
    id: "2477",
    name: "Visa Santander Débito",
    type: "Visa débito",
-   income: ["Pasividades BPS"],
-   services: ["ChatGPT Plus"],
-   linkedLoans: ["BROU Viaje Argentina", "BROU Dentista", "BROU Buenos Aires"]
+   income: ["Pasividades BPS", "Sueldo Elared", "Pagos Expande Digital"],
+   services: ["ChatGPT Plus"]
  },
  {
    id: "3879",
