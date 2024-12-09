f805c462998702f911a0e836397d501328302f62
N45h0 <70018907+N45h0@users.noreply.github.com>
2024-12-09 00:30:01 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,27 @@ import './index.css';
 import App from './App';
 import 'react-toastify/dist/ReactToastify.css';
 
+// Configuración de pdfjsLib
+import * as pdfjsLib from 'pdfjs-dist';
+pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.js`;
+
+// Configuración de Tesseract
+import { createWorker } from 'tesseract.js';
+window.createWorker = createWorker;
+
+// Configuración del worker de Tesseract
+const workerConfig = {
+  workerPath: '/tesseract/worker.min.js',
+  langPath: '/tesseract/lang-data',
+  corePath: '/tesseract/tesseract-core.wasm.js',
+  logger: m => console.log(m)
+};
+
+window.TESSERACT_CONFIG = workerConfig;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <React.StrictMode>
-   <App />
- </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
