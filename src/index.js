75f1f4c1dedd47e9516882eb2af6e9518e53bf11
N45h0 <70018907+N45h0@users.noreply.github.com>
2024-12-13 20:14:20 -0300

    Update index.js simple

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import './index.css';
 import App from './App';
 import 'react-toastify/dist/ReactToastify.css';
 
-// Configuración de pdfjsLib
-import * as pdfjsLib from 'pdfjs-dist';
-pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.js`;
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
